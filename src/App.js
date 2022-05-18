6cb1fb99d451d3a4f8a6bfd0ae84f5af9764ab46
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-05-19 05:10:11 +0530

    send changes in Groupselection

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" component={Home} title="BigAbout" />
+        <Route path="/" element={<Home />} />
       </Routes>
 
       <FooterSection></FooterSection>
---
+++ src/Container/GroupWebsiteComponent/GroupWebsiteComponent.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const ImageComponent = styled.div``;
+
+export const InformationComponent = styled.div``;
