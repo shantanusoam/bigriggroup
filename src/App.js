9173297ef513720e973dcd73d40429ff8e235609
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-05-19 01:42:54 +0530

    send changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import FooterSection from './Components/FooterSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn ReactW
-        </a> */}
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={Home} title="BigAbout" />
+      </Routes>
+
+      <FooterSection></FooterSection>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import GroupWebsitesSection from '../../Components/GroupWebsitesSection/GroupWebsitesSection';
+function Home() {
+  return <GroupWebsitesSection />;
+}
+export default Home;
