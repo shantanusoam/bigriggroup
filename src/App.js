3d37171865dc8255fa55449f73d2963552fe2e1e
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-05-19 01:11:56 +0530

    send change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn ReactW
+        </a> */}
       </header>
     </div>
   );
