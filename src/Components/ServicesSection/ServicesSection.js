ef14227ad5c382e6bbc5c524ec115669a72ab371
Kd <umarkhurshid3@gmail.com>
2022-05-20 03:19:54 +0530

    service update new

--- src/Components/ServicesSection/ServicesSection.js
+++ src/Components/ServicesSection/ServicesSection.js
@@ -1,93 +1,55 @@
-import React from 'react';
-import './data.js';
+import React from "react";
+import { ServiceComponentdata } from "../../data.js";
 
 export default function ServicesSection() {
-  return (
+  var dataservice = ServiceComponentdata[0];
+  console.log(dataservice);  
+  return ( 
     <>
       <div
         id="maincontainer"
         className="bg-Lightblue flex flex-col justify-between md:flex-row"
       >
-        <div id="firstmain" className="mt-48  xl:ml-60 ml-10 ">
+        <div id="firstmain" className="mt-56  xl:ml-60 ml-10 md:w-1/2 ">
           <div>
-            <h1 className="text-heading text-white font-heading ">
-              Our Services
+            <h1 className="text-heading text-white font-heading  ">
+              {dataservice.Heading}
             </h1>
             <p className="text-desc font-desc text-white w-2/3  ">
-              Kal Tires has 9 distribution centers, 4 service centers and
-              nationwide 24/7 emergency road service. Exclusive with major tire
-              manufactures such as Goodyear, Cooper, BKT, Michelin, Yokohama,
-              Double Coin, Pirelli and Hankook. We offer national account
-              billing for Goodyear, Cooper, Michelin, Yokohama and Hankook
-              products.{' '}
+              {dataservice.Desc}
             </p>
             <button className="text-white bg-yellow rounded p-4 w-44 mt-8">
-              CONTACT US
+              {dataservice.CTA.heading} 
+            
             </button>
           </div>
         </div>
+ 
+        <div id="Secondmain" className=" mt-24 pb-8 xl:ml-0 ml-10 w-1/2"> 
+          {dataservice.services.map((service) => (
+            <div
+              id="innermain"
+              className="flex justify-between mr-72 mt-16 "
+              key={service.id}
+            >
+              <div className="mr-6 mt-16">
+                <img className="mr-6" src={service.Logo} alt="truck" />
+              </div>
 
-        <div id="Secondmain" className=" mt-24 pb-8 xl:ml-0 ml-10 ">
-          <div id="innermain" className="flex justify-between mr-60">
-            <div className="mr-12 mt-16">
-              <img
-                className="mr-10"
-                src="https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/truck1white.svg"
-                alt=""
-              />
+              <div>
+                <h1 className="text-white text-subheading font-sub-heading">
+                  {service.Heading}
+                </h1>
+                <p className="text-desc font-desc text-white w-96 mt-4">
+                  {service.desc}
+                </p>
+              </div>
             </div>
-
-            <div>
-              <h1 className="text-white text-subheading font-sub-heading">
-                Trailers & Leasing
-              </h1>
-              <p className="text-desc font-desc text-white w-96 mt-4">
-                As a Vanguard authorized dealer, we offer a variety of dry vans
-                and refrigerated trailers and flatsbed and chessie.
-              </p>
-            </div>
-          </div>
-          <div id="innermain" className="flex justify-between mr-60 mt-16">
-            <div className="mr-12 mt-16">
-              <img
-                className="mr-10"
-                src="http://127.0.0.1:5500/src/assets/Images/white2.svg"
-                alt=""
-              />
-            </div>
-
-            <div>
-              <h1 className="text-white text-subheading font-sub-heading">
-                Partz
-              </h1>
-              <p className="text-desc font-desc text-white w-96 mt-4">
-                Designed To Reduce Your Cost Of Ownership, Our Trucks Improved
-                Fuel Economy, Enhanced Safety Features And Lead The Way In
-                Technological Advancements And Driver Comfort.
-              </p>
-            </div>
-          </div>
-          <div id="innermain" className="flex justify-between mr-60 mt-16">
-            <div className="mr-12 mt-16">
-              <img
-                className="mr-10   "
-                src="https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/white3.svg"
-                alt=""
-              />
-            </div>
-
-            <div>
-              <h1 className="text-white text-subheading font-sub-heading">
-                Tires & Services
-              </h1>
-              <p className="text-desc font-desc text-white w-96 mt-4 mb-16">
-                As a Vanguard authorized dealer, we offer a variety of dry vans
-                and refrigerated trailers and flatsbed and chessie.
-              </p>
-            </div>
-          </div>
+          ))}
+         
         </div>
       </div>
     </>
   );
 }
+
