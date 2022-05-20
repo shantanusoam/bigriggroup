a7ab4344f9ae77c65a017d30cdbbfb5c6def7fd1
Kd <umarkhurshid3@gmail.com>
2022-05-21 01:45:11 +0530

    header updat

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -13,7 +13,7 @@ import {
   NavLinks,
 } from "./NavbarElements";
 
-const Navbar = ({ toggle }) => {
+const   Navbar = ({ toggle }) => {
   const [scrollNav, setScrollNav] = useState(false); 
   const changeNav = () => {
     if (window.scrollY >= 100) {
@@ -37,11 +37,14 @@ const Navbar = ({ toggle }) => {
       <IconContext.Provider value={{ color: "#fff", size: "1em" }}>
         <Nav scrollNav={scrollNav}>
           <NavLogo to="/" onClick={toggleHome}>
-            <img
-              src="http://127.0.0.1:5500/src/assets/Images/navlogo.svg"
-              alt="logo"
-              width="80%"
-            />
+            <div>
+              <img
+                className="w-56 h-44"
+                src="http://127.0.0.1:5500/src/assets/Images/navlogo.svg"
+                alt="logo"
+                width="80%"
+              />
+            </div>
           </NavLogo>
 
           {/* <Bars onClick={toggle}/> */}
@@ -54,7 +57,7 @@ const Navbar = ({ toggle }) => {
 
             <NavItem>
               <NavLinks to="/" onClick={toggleHome}>
-                <a to="/">TRAILER & LEASING</a>  
+                <a to="/">TRAILER & LEASING</a>
               </NavLinks>
             </NavItem>
 
@@ -87,38 +90,25 @@ const Navbar = ({ toggle }) => {
 
             <NavItem>
               <NavLinks to="/Contact" onClick={toggleHome}>
-                <a to="/Contact">CONTACT</a> 
+                <a to="/Contact">CONTACT</a>
               </NavLinks>
             </NavItem>
 
             <NavItem>
               <NavLinks to="/Contact" onClick={toggleHome}>
-                <a to="/Contact">NEWS & EVENTS</a> 
+                <a to="/Contact">NEWS & EVENTS</a>
               </NavLinks>
             </NavItem>
-            {/* <NavItem>
-            
-          <NavLinks >
-          <Link to='ContactUs' smooth={true} duration={500} spy={true} exact={true} offset={-80}>Contact Us</Link>
-          </NavLinks>
-          </NavItem> */}
 
             <MobileIcon onClick={toggle}>
               <FaBars color="#111" />
             </MobileIcon>
-
-            {/* Second Nav */}
-            {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
           </NavMenu>
           <NavBtn>
-            <button className="text-white bg-yellow-bg p-2 px-16 font-semibold  rounded-md shadow-sm hover:shadow-md shadow-yellow-shadow hover:shadow-yellow-shadowhover text-2xl flex items-center justify-center">
+            <button className="text-white bg-yellow-bg w-80 h-16  font-semibold  rounded-md shadow-sm hover:shadow-md shadow-yellow-shadow hover:shadow-yellow-shadowhover text-2xl flex items-center justify-center ">
               (604) 864 3100
             </button>
-          
           </NavBtn>
-          {/* <MobileIcon onClick={toggle}>
-          <FaBars/>
-        </MobileIcon> */}
         </Nav>
       </IconContext.Provider>
     </>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import BrandComponent from '../../Components/BrandsComponent/BrandComponent';
 import GroupWebsitesSection from '../../Components/GroupWebsitesSection/GroupWebsitesSection';
+import HeroSection from '../../Components/HeaderHome';
 import Roadservice from '../../Components/RoadServiceComponent/Roadservice';
 import ServicesSection from '../../Components/ServicesSection/ServicesSection';
 function Home() {
   return (
     <>
+ <HeroSection></HeroSection>
       <GroupWebsitesSection />
       <Roadservice />
       <BrandComponent></BrandComponent>
