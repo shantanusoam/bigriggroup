e0653a01649e5e8e6162ccb870375afa9c1b412a
Kd <umarkhurshid3@gmail.com>
2022-05-26 02:55:55 +0530

    groupwebsitecom update

--- src/Components/RoadServiceComponent/Roadservice.js
+++ src/Components/RoadServiceComponent/Roadservice.js
@@ -29,7 +29,6 @@ export default function Roadservice() {
         </div>
 
         <img
-          className="md:-my-10  w-full 2xl:w-full lg:w-full "
           src="https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/2017-freightliner-cascadia-accessories_0%201.svg"
           alt="truck"
         />
