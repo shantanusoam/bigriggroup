30bf55898dd6312e8e9333f5294da1002a4f9ea0
Kd <umarkhurshid3@gmail.com>
2022-05-27 05:21:40 +0530

    red turck new

--- src/Container/ContactFormContainer/index.js
+++ src/Container/ContactFormContainer/index.js
@@ -5,9 +5,9 @@ import Formcomp from '../../Components/Contactsection/Formcomp';
 export const ContactFormContainer = () => {
   return (
     // <div id="maincontainer" className='w-70vh' >
-      <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-start lg:gap-12 flex  align-center justify-center ">
-        <div className="mt-24">
-          {/* <div>
+    <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-start lg:gap-12 flex  align-center justify-center ">
+      <div className="mt-24">
+        {/* <div>
           <h1 className="xl:text-5xl text-Heading font-semibold md:text-2xl link link-underline link-underline-black pb-8">
             Contact Form
           </h1>
@@ -16,24 +16,24 @@ export const ContactFormContainer = () => {
           </p>
         </div> */}
 
-          <Formcomp />
-          {/* <Contactform></Contactform> */}
-        </div>
+        <Formcomp />
+        {/* <Contactform></Contactform> */}
+      </div>
 
-        {/* <div className="flex ">
+      {/* <div className="flex ">
         <img
           src="https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/Partz.png"
           width="300"
         />
       </div> */}
-        {/* <div id="secondmain"> */}
-        <img
-          className=""
-          src="http://127.0.0.1:5500/src/assets/Images/redcontactimg.svg"
-          alt=""
-        />
-        {/* </div> */}
-      </div>
+      {/* <div id="secondmain"> */}
+      <img
+        className=""
+        src="https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/redtruck.svg"
+        alt=""
+      />
+      {/* </div> */}
+    </div>
     // </div>
   );
 };
