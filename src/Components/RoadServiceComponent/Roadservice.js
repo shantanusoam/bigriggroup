5e54bdc2d017d9ebc891995c496a2efd3434fdd6
Kd <umarkhurshid3@gmail.com>
2022-05-28 01:26:17 +0530

    pusdfio

--- src/Components/RoadServiceComponent/Roadservice.js
+++ src/Components/RoadServiceComponent/Roadservice.js
@@ -9,7 +9,7 @@ export default function Roadservice() {
             id="firtmaininner"
             className="xl:ml-48 lg:ml-10 mb-10 mt-10 2xl:pt-12 pt-0"
           >
-            <h1 className="text-white text-heading font-heading lg:text-2xl 2xl:pl-0 pl-6">
+            <h1 className="text-white text-heading font-heading lg:text-2xl 2xl:pl-0 pl-6">  
               24/7 Road Service
             </h1>
             <p className="text-white  font-desc 2xl:text-xl text-descnew 2xl:w-11/12 w-full 2xl:pl-0 pl-6 2xl:pt-4 pt-2">
