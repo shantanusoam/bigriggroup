184e108c4b300b01dccf51cb7203e5052def6d8f
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-05-28 02:01:09 +0530

    changwe

--- src/Components/Footernewcomp/Footer.js
+++ src/Components/Footernewcomp/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         id="maincontainer"
         className="bg-Lightblue "
         style={{
-          height: "50vh",
+          height: '50vh',
         }}
       >
         <div
@@ -20,24 +20,25 @@ export default function Footer() {
             className="flex-1 w-1/2 lg:block  hidden"
             style={{
               backgroundImage:
-                "url(" +
-                "https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/backmapfooter.svg" +
-                ")",
-              backgroundPosition: "center",
-              backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              width: "full",
-              marginRight: "5rem",
+                'url(' +
+                'https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/backmapfooter.svg' +
+                ')',
+              backgroundPosition: 'center',
+              backgroundSize: 'contain',
+              backgroundRepeat: 'no-repeat',
+              width: 'full',
+              marginRight: '5rem',
             }}
           >
             <div id="logo" className="ml-36">
               <img
                 src="https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/bigrigfooterlogo.png"
                 alt="logo"
+                width="200"
                 href="mailto:umarkhurshid3@gmail.com"
               />
             </div>
-            <div id="email" className="text-white flex ml-36">
+            <div id="email" className="text-white flex ml-36 mt-12">
               <div>
                 <img
                   src="https://raw.githubusercontent.com/shantanusoam/bigriggroup/master/src/assets/Images/Vectoremail.svg"
